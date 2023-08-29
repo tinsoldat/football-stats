584ae032430685f7388e1ff8ef40de1d080efdca
tinsoldat <lubsan03reg@gmail.com>
2023-08-29 19:57:16 +0800

    index page links

--- pages/index.vue
+++ pages/index.vue
@@ -2,4 +2,19 @@
 
 <template>
   <RouterView />
+  <h1>Thesis test task</h1>
+  <ul>
+    <li><a href="https://github.com/tinsoldat/football-stats">github</a></li>
+    <li>
+      <a
+        href="https://docs.google.com/document/d/1UM-hTYVYBKqlQnqsqEZwgn7Kj6u_x5Do8uyBhvSWkIQ"
+        >description</a
+      >
+    </li>
+    <li>
+      <a href="https://www.football-data.org/documentation/quickstart"
+        >api documentation</a
+      >
+    </li>
+  </ul>
 </template>
